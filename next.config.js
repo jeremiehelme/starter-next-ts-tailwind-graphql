a9a333a5b45fc82b8e5311cdedecb870ba456414
Jeremie Helme <jeremiehelme@gmail.com>
2023-09-08 13:09:01 +0200

    add nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    endpoint: "YOU_GRAPHQL_ENDPOINT",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "YOUR_HOSTNAME",
+        port: "",
+        pathname: "YOUR_PATHNAME/**",
+      },
+    ],
+    unoptimized: true,
+  }
+};
 module.exports = nextConfig
